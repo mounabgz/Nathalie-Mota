fd2c797d82428368beb26039e4613dccf773ed72
Mouna Aboulghazicd <mouna.abgz@gmail.com>
2024-11-21 13:47:54 +0100

    Correction des variables categorie et reference

--- template_part/Photo_block/photo_block.php
+++ template_part/Photo_block/photo_block.php
@@ -1,3 +1,4 @@
+<!-- PHOTOS APPARENTÉS  -->
 <div class= "photos_apparentes">
     <p> VOUS AIMEREZ AUSSI </p>
     <div class= "photos_apparentes-flex"> 
@@ -25,18 +26,20 @@ if ( !empty( $categorie ) && !is_wp_error( $categorie ) ) {
 
 // On exécute la WP Query
 $my_query = new WP_Query( $args );
-$categorie=strip_tags(get_the_term_list($post->ID, 'categorie'));
 
 // On lance la boucle
 if( $my_query->have_posts() ) : while( $my_query->have_posts() ) : $my_query->the_post();
+$categorie=strip_tags(get_the_term_list($post->ID, 'categorie')); // Récupère la catégorie
+$reference = get_field('reference', $post->ID); // Récupère la référence
 $post_id = get_permalink();
+$thumbnail_url= get_the_post_thumbnail_url();//variable qui recupère l'url de l'image pour ensuite être affiché dans la lightbox
     echo"<div class='photos_apparentes-images'>";
         echo "<a href='" . esc_url($post_id) . "'>";
             the_post_thumbnail('custom-thumbnail');
             echo "</a>";
                 echo "<div><i class='fa-regular fa-eye'></i>
-                    <i class='fa-solid fa-expand'></i>
-                    <p class='photos_apparentes-text'>REFERENCE DE LA PHOTO</p>
+                <i class='fa-solid fa-expand' data-url='{$thumbnail_url}' data-reference='{$reference}' data-categorie='{$categorie}'></i>
+                    <p class='photos_apparentes-text'>". $reference . "</p>
                     <p class='photos_apparentes-categorie'>" . $categorie . "</p>
                 </div>";
     echo"</div>";
